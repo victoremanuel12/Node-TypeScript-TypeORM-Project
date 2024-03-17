8f0b5ebf0ab6c00c62fd7372cedd3fb0a01fbe5f
Victor Emanuel <victoremannuel12@hotmail.com>
2024-03-17 19:05:59 -0300

    arquivo de migracao

---
+++ src/shared/typeorm/migrations/1710712890358-CreateProducts.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateProducts1710712890358 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
