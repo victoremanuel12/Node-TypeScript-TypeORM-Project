a4e05b8d91aed13581cea1d187a83dbeefbd8d66
Victor Emanuel <victoremannuel12@hotmail.com>
2024-05-09 08:51:30 -0300

    feat:inclusão do JWT no retorno da criação da sessão

--- src/modules/users/services/CreateSessionService.ts
+++ src/modules/users/services/CreateSessionService.ts
@@ -4,28 +4,39 @@ import { getCustomRepository } from 'typeorm';
 import User from '../typeorm/entities/User';
 import { UsersRepository } from '../typeorm/Repositories/UsersRepository';
 import AppError from '@shared/errors/appError';
-
+import { sign } from 'jsonwebtoken';
+import authConfig from '@config/auth';
 interface IRequest {
   email: string;
   password: string;
 }
+interface IResponse {
+  user: User;
+  token: string;
+}
 
 class CreateSessionService {
-  public async execute({ email, password }: IRequest): Promise<User> {
+  public async execute({ email, password }: IRequest): Promise<IResponse> {
     const usersRepository = getCustomRepository(UsersRepository);
     const user = await usersRepository.findByEmail(email);
 
     if (!user) {
       throw new AppError('Incorrect email/password combination.', 401);
     }
-
+    const token = sign({}, authConfig.jwt.secret, {
+      subject: user.id,
+      expiresIn: authConfig.jwt.expiresIn,
+    });
     const passwordConfirmed = await compare(password, user.password);
 
     if (!passwordConfirmed) {
       throw new AppError('Incorrect email/password combination.', 401);
     }
 
-    return user;
+    return{
+      user,
+      token
+    }
   }
 }
 
