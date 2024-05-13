d0c2a9af5cb0b44fe4ec6f10de1b9a3e03a69972
victoremannuel12 <victoremannuel12@hotmail.com>
2024-05-13 18:01:33 -0300

    feat: inicio do user token

---
+++ src/modules/userToken/typeorm/entities/usertoken.ts
@@ -0,0 +1,24 @@
+import { Entity, PrimaryGeneratedColumn, Column, Generated } from "typeorm"
+
+@Entity('userToken')
+ class UserToken {
+    @PrimaryGeneratedColumn('uuid')
+    id: string
+
+    @Column()
+    @Generated('uuid')
+    token: string;
+
+    @Column()
+    user_id: string;
+
+    @Column({ type: "timestamp", default: () => "CURRENT_TIMESTAMP" })
+    created_at: Date;
+
+    @Column({ type: "timestamp", default: () => "CURRENT_TIMESTAMP", onUpdate: "CURRENT_TIMESTAMP" })
+    updated_at: Date;
+}
+export default UserToken;
+
+
+
