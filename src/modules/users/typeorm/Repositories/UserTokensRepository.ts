3dab0cd4601b23dd9ac544c646ad9a72ca923ac6
Victor Emanuel <victoremanuel12@hotmail.com>
2024-05-26 23:36:53 -0300

    feat:criação do repositorio userToken

---
+++ src/modules/users/typeorm/Repositories/UserTokensRepository.ts
@@ -0,0 +1,28 @@
+import { EntityRepository, Repository } from 'typeorm';
+import UserToken from '../entities/UserToken';
+@EntityRepository(UserToken)
+export class UsersRepository extends Repository<UserToken> {
+  public async findByToken(token: string): Promise<UserToken | undefined> {
+    const tokenEntity = await this.findOne({
+      where: {
+        token,
+      },
+    });
+    return tokenEntity;
+  }
+  public async findById(Token_id: string): Promise<UserToken | undefined>  {
+    const product = await this.findOne({
+      where: {
+        Token_id,
+      },
+    });
+    return product;
+  }
+  public async generate(user_id : string ): Promise<UserToken | undefined> {
+    const tokenGenereted = await this.create({
+      user_id
+    })
+    this.save(tokenGenereted);
+    return tokenGenereted;
+  }
+}
