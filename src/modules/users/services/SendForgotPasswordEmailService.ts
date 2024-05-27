134ebbfb5962c0901741df853c269e5f188f59e6
Victor Emanuel <victoremanuel12@hotmail.com>
2024-05-27 00:45:51 -0300

    feat: correçãod e mensagem de erro

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -11,7 +11,7 @@ class SendForgotPasswordEmailService{
     const usersRepository = getCustomRepository(UsersRepository);
     const  userTokenRepository = getCustomRepository(UserTokenRepository);
     const user = await usersRepository.findByEmail(email);
-    if(!user) throw new AppError(`Could not find user with email ${email}`,404);
+    if(!user) throw new AppError(`Could not find user with email: ${email}`,404);
     const tokenGenereted  = await userTokenRepository.generate(user.id);
     console.log(tokenGenereted);
   }
